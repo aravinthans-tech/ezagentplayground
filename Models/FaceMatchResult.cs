2e53571397435b250df9e20a19b8aa73ffb06a3c
aravinthans-tech <aravinthan.s@ezofis.com>
2026-03-06 13:53:03 +0530

    newone

--- Models/FaceMatchResult.cs
+++ Models/FaceMatchResult.cs
@@ -3,6 +3,7 @@ namespace QRCodeAPI.Models;
 public class FaceMatchResult
 {
     public bool Match { get; set; }
+    public bool set { get; set; }
     public int MatchScore { get; set; }
     public string Message { get; set; } = string.Empty;
     public byte[]? LicenseFaceImage { get; set; }
